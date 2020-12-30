f1eb9b3bcc132a67a010e53e88e8bb62724d2e73
Pankaj <pkarora48216@gmail.com>
2020-12-30 22:49:14 +0530

    frontend app category created
    Signed-off-by: Pankaj <pkarora48216@gmail.com>

---
+++ mern-front-end-app/src/components/MenuHeader/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCategories } from "../../actions/index";
+import "./style.css";
+
+/**
+ * @author
+ * @function MenuHeader
+ **/
+
+const MenuHeader = (props) => {
+  const category = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+
+  function getCategories() {
+    dispatch(getAllCategories());
+  }
+
+  const renderCategories = (categories) => {
+    let myCategories = [];
+    for (let category of categories) {
+      myCategories.push(
+        <li key={category.name}>
+          {category.parentId ? (
+            <a href={category.slug}>{category.name}</a>
+          ) : (
+            <span>{category.name}</span>
+          )}
+          {category.children.length > 0 ? (
+            <ul>{renderCategories(category.children)}</ul>
+          ) : null}
+        </li>
+      );
+    }
+
+    return myCategories;
+  };
+
+  return (
+    <div className="menuHeader">
+      
+      <ul>
+        {category.categories.length > 0
+          ? renderCategories(category.categories)
+          : null}
+      </ul>
+      <button onClick={getCategories}>Get</button>
+    </div>
+  );
+};
+
+export default MenuHeader;
